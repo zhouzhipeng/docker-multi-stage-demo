431723a5336408fe98c446aeff2da328fbd2c31f
zhouzhipeng <admin@zhouzhipeng.com>
2018-02-24 11:34:21 +0800

    demo

---
+++ src/main/java/com/zhouzhipeng/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zhouzhipeng;
+
+/**
+ * TODO:写描述
+ * User: zhouzhipeng
+ * Date: 2018/2/24:11:32
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
